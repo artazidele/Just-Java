f7631e1897e11a67c951d467d937b0e750ba687b
artazidele <77989249+artazidele@users.noreply.github.com>
2021-10-10 11:19:12 +0300

    first lesson done

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -14,6 +14,8 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         int numberOfCoffees = 2;
         display(numberOfCoffees);
         displayPrice(numberOfCoffees * 5);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
     }
 
     private void display(int number) {
